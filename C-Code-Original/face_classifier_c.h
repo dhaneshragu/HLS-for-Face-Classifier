ce0366aabe7fa0831757b6031a0eb768fe24bb94
botketan <ketansingh918@gmail.com>
2024-03-26 17:38:01 +0530

    Original C-Code

---
+++ C-Code-Original/face_classifier_c.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void face_classifier_c(k2c_tensor* dense_input_input, k2c_tensor* activation_3_output); 
+void face_classifier_c_initialize(); 
+void face_classifier_c_terminate(); 
