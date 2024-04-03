12769cd0f125061f2937a2abad592c845f55c281
dhaneshragu <dhanesh.v@iitg.ac.in>
2024-04-03 21:20:34 +0530

    added model

---
+++ C-Code-Original/fake_img.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void fake_img(k2c_tensor* conv2d_input_input, k2c_tensor* dense_2_output); 
+void fake_img_initialize(); 
+void fake_img_terminate(); 
